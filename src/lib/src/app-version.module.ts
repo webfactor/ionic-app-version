d859931f88a100274fc79987b1d14ef03371d13c
Christian Kasperbauer <christian.kasperbauer@webfactor.de>
2018-04-10 14:51:14 +0200

    initial commit

---
+++ src/lib/src/app-version.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { AppVersion } from '@ionic-native/app-version';
+
+import { AppVersionComponent } from './app-version.module';
+
+export * from './components/app-version';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [AppVersionComponent],
+    exports: [AppVersionComponent, AppVersion]
+})
+export class AppVersionModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: AppVersionModule
+            // providers: [AppVersion]
+        };
+    }
+}
---
+++ src/lib/src/index.ts
@@ -0,0 +1,2 @@
+export * from './app-version.module';
+export * from './components/app-version';
